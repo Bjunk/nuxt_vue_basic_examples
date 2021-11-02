39f277bb1aa002366c71d6d3f3a95eb2906f1317
Bjunk <pablo.aravenam@gmail.com>
2021-11-02 10:25:33 -0300

    new_example

---
+++ test1-prop/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header>
+        <h1>Header View</h1>
+        <nav>
+            <ul>
+                <li>Home
+                </li>
+                <li>Products
+                </li>
+                <li>Carts {{ cartnum }}
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+<script>
+export default {
+    props: ['cartnum'],
+    data () {
+        return {
+            counter: this.cartnum
+        }
+    }
+}
+</script>
---
+++ test1-prop/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <Header :cartnum="update"></Header>
+    <h1>Product View</h1>
+    <p><span @click="cartNumber++">+</span><input v-model="cartNumber"><span @click="cartNumber--">-</span></p>
+    <button @click="updateCart">Add to Cart</button>
+  </section>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+
+export default {
+  name: 'index',
+  components: { Header },
+  data () {
+    return {
+      cartNumber: 0,
+      update: 0
+    }
+  },
+  methods: {
+    updateCart () {
+        return this.update = this.cartNumber;
+    }
+  }
+
+
+}
+</script>
